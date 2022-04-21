cfcb56b03f0bdf6803ffc2e84460e0588ea8a261
Tishtanya Shaw <Tishtanyaa@icloud.com>
2022-04-21 01:20:08 -0400

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Routes, Route} from 'react-router-dom';
 import Header  from './components/Header';
+import HomePage from './pages/HomePage';
 import './App.css';
 
 function App() {
   
 	return (
 			<div>
-				<Header />  
+				<Header />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>  
 			</div>
 
 	);
